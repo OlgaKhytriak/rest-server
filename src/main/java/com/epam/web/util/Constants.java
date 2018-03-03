dee33771e7c09f42722f46a62cdd14024f806410
OlgaKhytriak <KhytriakOlga@gmail.com>
2018-03-03 12:53:53 +0200

    Const

---
+++ src/main/java/com/epam/web/util/Constants.java
@@ -0,0 +1,17 @@
+package com.epam.web.util;
+
+public class Constants {
+    private Constants() {
+    }
+
+    public static final String INCORRECT_INPUT_VALUE = "Incorrect input value: ";
+    public static final String NEWS_BY_ID_NOT_FOUND = "News was not found";
+    public static final String NO_INPUT_PARAMETERS = "There are not input parameters: ";
+    //rewrite
+    public static final String BOOK_BY_NAME_OR_AUTHOR_NOT_FOUND = "There are no books with such parametres: ";
+
+    public static final String NEWS_WAS_ADDED = "News was added successfully";
+    public static final String NEWS_WAS_UPDATED = "News was updated successfully";
+    public static final String NEWS_WAS_DELETED = "News was deleted successfully";
+
+}
