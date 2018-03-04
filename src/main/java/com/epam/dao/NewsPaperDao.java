1c483a5f51f5e0a87901a2780d41722ccdbf22a4
OlgaKhytriak <KhytriakOlga@gmail.com>
2018-03-04 16:00:43 +0200

    List of real news

--- src/main/java/com/epam/dao/NewsPaperDao.java
+++ src/main/java/com/epam/dao/NewsPaperDao.java
@@ -15,9 +15,17 @@ public class NewsPaperDao {
     }
 
     private void init(){
-        newsList.add(new SingleNews(1,"News1","sport","Some text of news","http://ur1 1"));
-        newsList.add(new SingleNews(2,"News2","science","Some text of news","http://url 2"));
-        newsList.add(new SingleNews(3,"News3","society","Some text of news","http://url 3"));
+        newsList.add(new SingleNews(1,"Gold-medal","Sport","Gold medalist Abramenko carries Ukrainian flag at Winter Olympics 2018 closing ceremony","https://24tv.ua"));
+        newsList.add(new SingleNews(2,"Big-Bang","Science","At the time of the Big Bang, all the matter in the universe was smooshed into an incredibly hot, infinitely dense speck of matter.","https://www.ukr.net"));
+        newsList.add(new SingleNews(3,"Luxury-labels","Society","The luxury labels coming out of Africa","http://www.bbc.com"));
+        newsList.add(new SingleNews(4,"Box","Sport","Ukrainian Artem Dalakian becomes new WBA flyweight champion","https://zik.ua"));
+        newsList.add(new SingleNews(5,"Dark-Wave","Science","At the edge of life and death is a spreading dark wave","https://newsone.ua"));
+        newsList.add(new SingleNews(6,"Heart-attack","Health","Waist size bigger heart attack risk in women, report says","https://www.telegraph.co.uk"));
+        newsList.add(new SingleNews(7,"Olympics","Sport","Winter Olympics 2018 Schedule of Ukrainian team","https://news.sky.com"));
+        newsList.add(new SingleNews(8,"Religious-Mummies","Science","At least 40 limestone sarcophagi that held mummified burials were discovered in the cemetery.","https://www.express.co.uk"));
+        newsList.add(new SingleNews(9,"Penguin-colony","Nature","Penguin super-colony spotted from space","http://www.bbc.com/news"));
+        newsList.add(new SingleNews(10,"Shakhtar","Sport","Shakhtar Donetsk beats Roma","https://www.theverge.com"));
+
     }
 
     public void add(SingleNews news){
@@ -104,12 +112,7 @@ public class NewsPaperDao {
             newsList.add(newSingleNews);
         }
     }
-
-
-
-
-
-    public List<SingleNews> getAllByAuthor(String category){//перейменувати на катерогію
+   public List<SingleNews> getAllByAuthor(String category){//перейменувати на катерогію
         List<SingleNews> list = new ArrayList<>();
         for (SingleNews singleNews : newsList) {
             if(singleNews.getCategory().equals(category)){
